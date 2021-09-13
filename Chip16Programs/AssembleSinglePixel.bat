5686e7e4acb0bdcb436d2451369c16fc94da155f
unknown <magnus@wedmark.se>
2021-09-12 23:43:24 -0700

    Fixed incorrect relative path to Bin2Coe

--- Chip16Programs/AssembleSinglePixel.bat
+++ Chip16Programs/AssembleSinglePixel.bat
@@ -1,5 +1,5 @@
 cd SinglePixel
 ..\tchip16.exe SinglePixel.asm -o SinglePixel.c16 -m
 copy mmap.txt SinglePixel.txt
-..\Bin2Coe\bin\Release\Bin2Coe.exe SinglePixel.c16
+..\..\Bin2Coe\bin\Release\Bin2Coe.exe SinglePixel.c16
 cd ..
